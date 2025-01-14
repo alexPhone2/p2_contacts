a966159987d16af9f5ce4b414804f70f07314b14
alexPhone2 <alex@phone2.io>
2025-01-14 11:20:05 +0300

    Release v1.0.2

--- contacts/contact.go
+++ contacts/contact.go
@@ -1,11 +1,13 @@
 package contacts
 
 type Contact struct {
-	Name string
+	Name        string
+	PhoneNumber string
 }
 
 func GetContact() Contact {
 	return Contact{
-		Name: "John Doe",
+		Name:        "John Doe",
+		PhoneNumber: "123-456-7890",
 	}
 }
